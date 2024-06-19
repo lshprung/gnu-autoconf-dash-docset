b949528c9a8bb9c3c7bd6723bd81d2c917b6d50e
Louie S <louie@example.com>
2024-06-19 17:02:00 -0400

    Update script

--- config.mk
+++ config.mk
@@ -11,15 +11,15 @@ $(DOCUMENTS_DIR): $(RESOURCES_DIR) $(MANUAL_FILE)
 	mkdir -p $@
 	tar -x -z -f $(MANUAL_FILE) -C $@
 
-$(INDEX_FILE): $(SOURCE_DIR)/src/index.sh $(DOCUMENTS_DIR)
+$(INDEX_FILE): $(SOURCE_DIR)/src/index-pages.sh $(SOURCE_DIR)/src/index-terms.sh $(DOCUMENTS_DIR)
 	rm -f $@
-	$(SOURCE_DIR)/src/index.sh $@ $(DOCUMENTS_DIR)/*.html
-	$(SOURCE_DIR)/src/index.sh -i "Entry" $@ $(DOCUMENTS_DIR)/Concept-Index.html
-	$(SOURCE_DIR)/src/index.sh -i "Macro" $@ $(DOCUMENTS_DIR)/M4-Macro-Index.html
-	$(SOURCE_DIR)/src/index.sh -i "Macro" $@ $(DOCUMENTS_DIR)/Autoconf-Macro-Index.html
-	$(SOURCE_DIR)/src/index.sh -i "Macro" $@ $(DOCUMENTS_DIR)/Autotest-Macro-Index.html
-	$(SOURCE_DIR)/src/index.sh -i "Variable" $@ $(DOCUMENTS_DIR)/Cache-Variable-Index.html
-	$(SOURCE_DIR)/src/index.sh -i "Variable" $@ $(DOCUMENTS_DIR)/Output-Variable-Index.html
-	$(SOURCE_DIR)/src/index.sh -i "Function" $@ $(DOCUMENTS_DIR)/Program-_0026-Function-Index.html
-	$(SOURCE_DIR)/src/index.sh -i "Entry" $@ $(DOCUMENTS_DIR)/Preprocessor-Symbol-Index.html
-	$(SOURCE_DIR)/src/index.sh -i "Variable" $@ $(DOCUMENTS_DIR)/Environment-Variable-Index.html
+	$(SOURCE_DIR)/src/index-pages.sh $@ $(DOCUMENTS_DIR)/*.html
+	$(SOURCE_DIR)/src/index-terms.sh "Entry" $@ $(DOCUMENTS_DIR)/Concept-Index.html
+	$(SOURCE_DIR)/src/index-terms.sh "Macro" $@ $(DOCUMENTS_DIR)/M4-Macro-Index.html
+	$(SOURCE_DIR)/src/index-terms.sh "Macro" $@ $(DOCUMENTS_DIR)/Autoconf-Macro-Index.html
+	$(SOURCE_DIR)/src/index-terms.sh "Macro" $@ $(DOCUMENTS_DIR)/Autotest-Macro-Index.html
+	$(SOURCE_DIR)/src/index-terms.sh "Variable" $@ $(DOCUMENTS_DIR)/Cache-Variable-Index.html
+	$(SOURCE_DIR)/src/index-terms.sh "Variable" $@ $(DOCUMENTS_DIR)/Output-Variable-Index.html
+	$(SOURCE_DIR)/src/index-terms.sh "Function" $@ $(DOCUMENTS_DIR)/Program-_0026-Function-Index.html
+	$(SOURCE_DIR)/src/index-terms.sh "Entry" $@ $(DOCUMENTS_DIR)/Preprocessor-Symbol-Index.html
+	$(SOURCE_DIR)/src/index-terms.sh "Variable" $@ $(DOCUMENTS_DIR)/Environment-Variable-Index.html
---
+++ src/index-pages.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+
+# shellcheck source=../../../scripts/create_table.sh
+. "$(dirname "$0")"/../../../scripts/create_table.sh
+# shellcheck source=../../../scripts/insert.sh
+. "$(dirname "$0")"/../../../scripts/insert.sh
+
+DB_PATH="$1"
+shift
+
+get_title() {
+	FILE="$1"
+
+	pup -p -f "$FILE" 'title text{}' | \
+		tr -d \\n | \
+		sed 's/ (Autoconf)//g' | \
+		sed 's/\"/\"\"/g'
+}
+
+insert_pages() {
+	# Get title and insert into table for each html file
+	while [ -n "$1" ]; do
+		unset PAGE_NAME
+		unset PAGE_TYPE
+		PAGE_NAME="$(get_title "$1")"
+		PAGE_TYPE="Guide"
+		if [ -n "$PAGE_NAME" ]; then
+			insert "$DB_PATH" "$PAGE_NAME" "$PAGE_TYPE" "$(basename "$1")"
+		fi
+		shift
+	done
+}
+
+create_table "$DB_PATH"
+insert_pages "$@"
