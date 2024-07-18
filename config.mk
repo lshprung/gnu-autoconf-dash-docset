3d2ab8b331227610eea27fefe21f7680bce3c232
Louie S <louie@example.com>
2024-07-18 17:56:46 -0400

    Accomodate modification to index-terms.sh

--- config.mk
+++ config.mk
@@ -1,4 +1,5 @@
 SRC_ICON_FILE=$(SOURCE_DIR)/icon.png
+INDEX_ENTRY_CLASS=\"printindex-index-entry\"
 
 ifdef VERSION
 	MANUAL_URL = https://www.gnu.org/savannah-checkouts/gnu/autoconf/manual/autoconf-${VERSION}/autoconf.html_node.tar.gz
@@ -14,15 +15,15 @@ $(DOCUMENTS_DIR): $(RESOURCES_DIR) $(MANUAL_FILE)
 	mkdir -p $@
 	tar -x -z -f $(MANUAL_FILE) -C $@
 
-$(INDEX_FILE): $(SOURCE_DIR)/src/index-pages.sh $(SCRIPTS_DIR)/gnu/index-terms.sh $(DOCUMENTS_DIR)
+$(INDEX_FILE): $(SOURCE_DIR)/src/index-pages.sh $(SCRIPTS_DIR)/gnu/index-terms-class.sh $(DOCUMENTS_DIR)
 	rm -f $@
 	$(SOURCE_DIR)/src/index-pages.sh $@ $(DOCUMENTS_DIR)/*.html
-	$(SCRIPTS_DIR)/gnu/index-terms.sh "Entry" $@ $(DOCUMENTS_DIR)/Concept-Index.html
-	$(SCRIPTS_DIR)/gnu/index-terms.sh "Macro" $@ $(DOCUMENTS_DIR)/M4-Macro-Index.html
-	$(SCRIPTS_DIR)/gnu/index-terms.sh "Macro" $@ $(DOCUMENTS_DIR)/Autoconf-Macro-Index.html
-	$(SCRIPTS_DIR)/gnu/index-terms.sh "Macro" $@ $(DOCUMENTS_DIR)/Autotest-Macro-Index.html
-	$(SCRIPTS_DIR)/gnu/index-terms.sh "Variable" $@ $(DOCUMENTS_DIR)/Cache-Variable-Index.html
-	$(SCRIPTS_DIR)/gnu/index-terms.sh "Variable" $@ $(DOCUMENTS_DIR)/Output-Variable-Index.html
-	$(SCRIPTS_DIR)/gnu/index-terms.sh "Function" $@ $(DOCUMENTS_DIR)/Program-_0026-Function-Index.html
-	$(SCRIPTS_DIR)/gnu/index-terms.sh "Entry" $@ $(DOCUMENTS_DIR)/Preprocessor-Symbol-Index.html
-	$(SCRIPTS_DIR)/gnu/index-terms.sh "Variable" $@ $(DOCUMENTS_DIR)/Environment-Variable-Index.html
+	$(SCRIPTS_DIR)/gnu/index-terms-class.sh "Entry"    $(INDEX_ENTRY_CLASS) $@ $(DOCUMENTS_DIR)/Concept-Index.html
+	$(SCRIPTS_DIR)/gnu/index-terms-class.sh "Macro"    $(INDEX_ENTRY_CLASS) $@ $(DOCUMENTS_DIR)/M4-Macro-Index.html
+	$(SCRIPTS_DIR)/gnu/index-terms-class.sh "Macro"    $(INDEX_ENTRY_CLASS) $@ $(DOCUMENTS_DIR)/Autoconf-Macro-Index.html
+	$(SCRIPTS_DIR)/gnu/index-terms-class.sh "Macro"    $(INDEX_ENTRY_CLASS) $@ $(DOCUMENTS_DIR)/Autotest-Macro-Index.html
+	$(SCRIPTS_DIR)/gnu/index-terms-class.sh "Variable" $(INDEX_ENTRY_CLASS) $@ $(DOCUMENTS_DIR)/Cache-Variable-Index.html
+	$(SCRIPTS_DIR)/gnu/index-terms-class.sh "Variable" $(INDEX_ENTRY_CLASS) $@ $(DOCUMENTS_DIR)/Output-Variable-Index.html
+	$(SCRIPTS_DIR)/gnu/index-terms-class.sh "Function" $(INDEX_ENTRY_CLASS) $@ $(DOCUMENTS_DIR)/Program-_0026-Function-Index.html
+	$(SCRIPTS_DIR)/gnu/index-terms-class.sh "Entry"    $(INDEX_ENTRY_CLASS) $@ $(DOCUMENTS_DIR)/Preprocessor-Symbol-Index.html
+	$(SCRIPTS_DIR)/gnu/index-terms-class.sh "Variable" $(INDEX_ENTRY_CLASS) $@ $(DOCUMENTS_DIR)/Environment-Variable-Index.html
