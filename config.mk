3a2e82e05bb20eae4545967a6cd83a4e1aaa7a98
Louie Shprung <lshprung@tutanota.com>
2024-12-18 13:20:39 -0500

    Change shared script name

--- config.mk
+++ config.mk
@@ -14,15 +14,15 @@ $(DOCUMENTS_DIR): $(RESOURCES_DIR) $(MANUAL_FILE)
 	mkdir -p $@
 	tar -x -z -f $(MANUAL_FILE) -C $@
 
-$(INDEX_FILE): $(SOURCE_DIR)/src/index-pages.sh $(SCRIPTS_DIR)/gnu/index-terms.sh $(DOCUMENTS_DIR)
+$(INDEX_FILE): $(SOURCE_DIR)/src/index-pages.sh $(SCRIPTS_DIR)/gnu/index-terms-colon.sh $(DOCUMENTS_DIR)
 	rm -f $@
 	$(SOURCE_DIR)/src/index-pages.sh $@ $(DOCUMENTS_DIR)/*.html
-	$(SCRIPTS_DIR)/gnu/index-terms.sh "Entry" $@ $(DOCUMENTS_DIR)/Concept-Index.html
-	$(SCRIPTS_DIR)/gnu/index-terms.sh "Macro" $@ $(DOCUMENTS_DIR)/M4-Macro-Index.html
-	$(SCRIPTS_DIR)/gnu/index-terms.sh "Macro" $@ $(DOCUMENTS_DIR)/Autoconf-Macro-Index.html
-	$(SCRIPTS_DIR)/gnu/index-terms.sh "Macro" $@ $(DOCUMENTS_DIR)/Autotest-Macro-Index.html
-	$(SCRIPTS_DIR)/gnu/index-terms.sh "Variable" $@ $(DOCUMENTS_DIR)/Cache-Variable-Index.html
-	$(SCRIPTS_DIR)/gnu/index-terms.sh "Variable" $@ $(DOCUMENTS_DIR)/Output-Variable-Index.html
-	$(SCRIPTS_DIR)/gnu/index-terms.sh "Function" $@ $(DOCUMENTS_DIR)/Program-_0026-Function-Index.html
-	$(SCRIPTS_DIR)/gnu/index-terms.sh "Entry" $@ $(DOCUMENTS_DIR)/Preprocessor-Symbol-Index.html
-	$(SCRIPTS_DIR)/gnu/index-terms.sh "Variable" $@ $(DOCUMENTS_DIR)/Environment-Variable-Index.html
+	$(SCRIPTS_DIR)/gnu/index-terms-colon.sh "Entry" $@ $(DOCUMENTS_DIR)/Concept-Index.html
+	$(SCRIPTS_DIR)/gnu/index-terms-colon.sh "Macro" $@ $(DOCUMENTS_DIR)/M4-Macro-Index.html
+	$(SCRIPTS_DIR)/gnu/index-terms-colon.sh "Macro" $@ $(DOCUMENTS_DIR)/Autoconf-Macro-Index.html
+	$(SCRIPTS_DIR)/gnu/index-terms-colon.sh "Macro" $@ $(DOCUMENTS_DIR)/Autotest-Macro-Index.html
+	$(SCRIPTS_DIR)/gnu/index-terms-colon.sh "Variable" $@ $(DOCUMENTS_DIR)/Cache-Variable-Index.html
+	$(SCRIPTS_DIR)/gnu/index-terms-colon.sh "Variable" $@ $(DOCUMENTS_DIR)/Output-Variable-Index.html
+	$(SCRIPTS_DIR)/gnu/index-terms-colon.sh "Function" $@ $(DOCUMENTS_DIR)/Program-_0026-Function-Index.html
+	$(SCRIPTS_DIR)/gnu/index-terms-colon.sh "Entry" $@ $(DOCUMENTS_DIR)/Preprocessor-Symbol-Index.html
+	$(SCRIPTS_DIR)/gnu/index-terms-colon.sh "Variable" $@ $(DOCUMENTS_DIR)/Environment-Variable-Index.html
