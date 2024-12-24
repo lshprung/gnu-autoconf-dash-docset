8cd7ee7c6cb26e0866dee340c9d5680f37ff2d3c
Louie Shprung <lshprung@tutanota.com>
2024-12-24 08:27:08 -0800

    python rewrite; use index-terms instead of index-terms-colon

--- config.mk
+++ config.mk
@@ -1,4 +1,5 @@
 SRC_ICON_FILE=$(SOURCE_DIR)/icon.png
+INDEX_ENTRY_CLASS=printindex-index-entry
 
 ifdef VERSION
 	MANUAL_URL = https://www.gnu.org/savannah-checkouts/gnu/autoconf/manual/autoconf-${VERSION}/autoconf.html_node.tar.gz
@@ -14,15 +15,14 @@ $(DOCUMENTS_DIR): $(RESOURCES_DIR) $(MANUAL_FILE)
 	mkdir -p $@
 	tar -x -z -f $(MANUAL_FILE) -C $@
 
-$(INDEX_FILE): $(SOURCE_DIR)/src/index-pages.sh $(SCRIPTS_DIR)/gnu/index-terms-colon.sh $(DOCUMENTS_DIR)
+$(INDEX_FILE): $(SOURCE_DIR)/src/index-pages.py $(SCRIPTS_DIR)/gnu/index-terms.py $(DOCUMENTS_DIR)
 	rm -f $@
-	$(SOURCE_DIR)/src/index-pages.sh $@ $(DOCUMENTS_DIR)/*.html
-	$(SCRIPTS_DIR)/gnu/index-terms-colon.sh "Entry" $@ $(DOCUMENTS_DIR)/Concept-Index.html
-	$(SCRIPTS_DIR)/gnu/index-terms-colon.sh "Macro" $@ $(DOCUMENTS_DIR)/M4-Macro-Index.html
-	$(SCRIPTS_DIR)/gnu/index-terms-colon.sh "Macro" $@ $(DOCUMENTS_DIR)/Autoconf-Macro-Index.html
-	$(SCRIPTS_DIR)/gnu/index-terms-colon.sh "Macro" $@ $(DOCUMENTS_DIR)/Autotest-Macro-Index.html
-	$(SCRIPTS_DIR)/gnu/index-terms-colon.sh "Variable" $@ $(DOCUMENTS_DIR)/Cache-Variable-Index.html
-	$(SCRIPTS_DIR)/gnu/index-terms-colon.sh "Variable" $@ $(DOCUMENTS_DIR)/Output-Variable-Index.html
-	$(SCRIPTS_DIR)/gnu/index-terms-colon.sh "Function" $@ $(DOCUMENTS_DIR)/Program-_0026-Function-Index.html
-	$(SCRIPTS_DIR)/gnu/index-terms-colon.sh "Entry" $@ $(DOCUMENTS_DIR)/Preprocessor-Symbol-Index.html
-	$(SCRIPTS_DIR)/gnu/index-terms-colon.sh "Variable" $@ $(DOCUMENTS_DIR)/Environment-Variable-Index.html
+	$(SOURCE_DIR)/src/index-pages.py $@ $(DOCUMENTS_DIR)/*.html
+	$(SCRIPTS_DIR)/gnu/index-terms.py "Entry"    $(INDEX_ENTRY_CLASS) $@ $(DOCUMENTS_DIR)/Concept-Index.html
+	$(SCRIPTS_DIR)/gnu/index-terms.py "Macro"    $(INDEX_ENTRY_CLASS) $@ $(DOCUMENTS_DIR)/M4-Macro-Index.html
+	$(SCRIPTS_DIR)/gnu/index-terms.py "Macro"    $(INDEX_ENTRY_CLASS) $@ $(DOCUMENTS_DIR)/Autoconf-Macro-Index.html
+	$(SCRIPTS_DIR)/gnu/index-terms.py "Variable" $(INDEX_ENTRY_CLASS) $@ $(DOCUMENTS_DIR)/Cache-Variable-Index.html
+	$(SCRIPTS_DIR)/gnu/index-terms.py "Variable" $(INDEX_ENTRY_CLASS) $@ $(DOCUMENTS_DIR)/Output-Variable-Index.html
+	$(SCRIPTS_DIR)/gnu/index-terms.py "Function" $(INDEX_ENTRY_CLASS) $@ $(DOCUMENTS_DIR)/Program-_0026-Function-Index.html
+	$(SCRIPTS_DIR)/gnu/index-terms.py "Entry"    $(INDEX_ENTRY_CLASS) $@ $(DOCUMENTS_DIR)/Preprocessor-Symbol-Index.html
+	$(SCRIPTS_DIR)/gnu/index-terms.py "Variable" $(INDEX_ENTRY_CLASS) $@ $(DOCUMENTS_DIR)/Environment-Variable-Index.html
